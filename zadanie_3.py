f15daee54817283a302cdbbab9f7e45719026688
Modiga <147749973+Modiga@users.noreply.github.com>
2023-10-12 20:13:28 +0200

    Create zadanie_3.py
    dodaje zadanie 3

---
+++ zadanie_3.py
@@ -0,0 +1,12 @@
+# calkowite i niecalkowite
+pierwsza = 3
+druga = 5
+trzecia = pierwsza/druga
+
+print(f"Liczby calkowite: {pierwsza}, {druga}\nLiczba niecalkowita: {trzecia}")
+
+# dwie zmienne przechowujace slowa
+tekst1 = "Currently, artificial intelligence is increasingly used to"
+tekst2 = "recognize and differentiate emotions."
+
+print(f"{tekst1} {tekst2}")
