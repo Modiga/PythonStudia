50f50987fe0aefcd07c1d6580092d52bcfd37295
Modiga <147749973+Modiga@users.noreply.github.com>
2023-12-09 17:02:56 +0100

    HANOI wjeżdża!
    AU!

---
+++ wieza_hanoi_zad_12.py
@@ -0,0 +1,16 @@
+def hanoi(n, zrodlo, cel, pomoc):
+    if n > 0:
+        # Przenieś n-1 krążków z źródła do pomocniczego
+        hanoi(n-1, zrodlo, pomoc, cel)
+        
+        # Przenieś n-ty krążek z źródła do celu
+        print(f"Przenieś krążek {n} z {zrodlo} do {cel}")
+        
+        # Przenieś n-1 krążków z pomocniczego do celu
+        hanoi(n-1, pomoc, cel, zrodlo)
+
+# Przykład użycia dla x krążków na wieży S, przenosząc je na wieżę C, z użyciem wieży P jako pomocniczej
+hanoi(3, 'S', 'C', 'P')
+
+
+# Wideo: https://youtu.be/iG4lHkfuu8I?t=618
